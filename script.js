38f392ebb236c40864231cdef6a09b7be7b5fe3e
Wojciech Sopel <wojciech.sopel24@my.northampton.ac.uk>
2025-05-29 17:47:09 +0100

    mewo

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 // rock paper scisers 
+let rock = document.getElementById("rock")
+//rock is 1
+let paper = document.getElementById("paper")
+//paper is 2
+let sis = document.getElementById("sis")
+//sis is 3 
+let playerdes = 0;
+function play(playerdes){
+    let botdes = Math.ceil(Math.random() * 3)
+    if (botdes == playerdes){
+        draw()
+    }
+    else if(playerdes == 1 && botdes == 2||
+        playerdes == 2 && botdes == 3 ||
+        playerdes == 3 && botdes == 1
+    ){
+        lose()
+    }
+    else if(playerdes == 2 && botdes == 1 ||
+        playerdes == 3 && botdes == 2 ||
+        playerdes == 1 && botdes == 3
+    ){
+        win()
+    }
+} 
+function lose(){
+    alert("lose")
+}
+function win(){
+    alert("win")
+}
+function draw(){
+    alert("draw")
+}
+rock.addEventListener("click", play(1))
+paper.addEventListener("click", play(2))
+sis.addEventListener("click", play(3))
